8758bf87003117e12764064bbdfc59142767bbe2
alex <maisilalexander@gmail.com>
2024-06-05 22:21:53 +0300

    home component added

--- firstProject/src/app/app.routes.ts
+++ firstProject/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import path from 'node:path';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path:'home',component:HomeComponent}];
